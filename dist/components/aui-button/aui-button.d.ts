cdb4bd6374b87e720219c8c8a3f417e8094fb631
miura738 <miura738@1oli.cn>
2024-01-03 16:29:23 +0800

    删了多余的东西

--- dist/components/aui-button/aui-button.d.ts
+++
@@ -1,9 +0,0 @@
-import React from "react";
-export interface AuiButtonProps {
-    theme?: "light" | "dark" | "success" | "error" | "danger" | "allow";
-    children?: string;
-    className?: string;
-    onClick?: () => void;
-    type?: "button" | "submit";
-}
-export declare const AuiButton: ({ children, className, theme, ...props }: AuiButtonProps) => React.JSX.Element;
